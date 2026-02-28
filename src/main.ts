b181bf9c353cf927fe5fae0d659e0eb0f7939c7f
meftunca <buraksenturk25@gmail.com>
2026-02-28 17:25:09 +0300

    Sync

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { Analytics } from "@vercel/analytics/vue"
 
 const app = createApp(App)
 app.use(router)
