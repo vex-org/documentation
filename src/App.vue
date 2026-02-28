b3dd7716301b17b42054e8e8dbc00d82eb511be1
meftunca <buraksenturk25@gmail.com>
2026-02-28 17:37:41 +0300

    Sync

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import AppLayout from './components/AppLayout.vue'
+import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { Analytics } from "@vercel/analytics/vue"
+
 </script>
 
 <template>
   <AppLayout>
     <RouterView />
+    <SpeedInsights />
+    <Analytics />
   </AppLayout>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
-import { SpeedInsights } from "@vercel/speed-insights/vue"
-import { Analytics } from "@vercel/analytics/vue"
 
 const app = createApp(App)
 app.use(router)
